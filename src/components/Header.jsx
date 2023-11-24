83836681f63e7a8a55adb3076e5b0ee2eee9978a
Mukesh Kumar <mukeshkumar117733@gmail.com>
2023-11-24 19:09:51 +0530

    Changed The Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header(){
     return (
         <div className="border  drop-shadow shadow-md mb-6 py-2 w-full fixed top-0 opacity-100 bg-white">
             <header className="text-center">
-                <h1 className="text-2xl font-extrabold uppercase">Codehelp Blogs</h1>
+                <h1 className="text-2xl font-extrabold uppercase">Blogs</h1>
             </header>
         </div>
     )
